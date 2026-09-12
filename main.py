0469e89172feb1a9829070550537a0f5ed89ac39
Desentom <axelitvari@gmail.com>
2026-09-12 14:55:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import socket
 import threading
-import base64
-import html
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import requests.packages.urllib3.util.connection as urllib_conn
 
@@ -11,29 +9,14 @@ def allowed_gai_family():
 urllib_conn.allowed_gai_family = allowed_gai_family
 
 import telebot
+import html
 
-# Сервер VLESS Desentom VPN
-STATIC_SERVER_KEY = (
-    "vless://342f746b-26f3-4093-b87e-bfdb6a38def2@178.248.236.7:50443"
-    "?security=reality&encryption=none&pbk=r-_zNu0BrD8PgGzg-KQSes06Si83txHWYPH-o8xXihk"
-    "&fp=qq&type=tcp&flow=xtls-rprx-vision&sni=rutube.ru&sid=a66ffc56b634bd44#Desentom%20VPN"
-)
-
-SUB_URL = 'https://ancient-forest-b030.axelitvari.workers.dev/'
-
-# Встроенный веб-сервер (Health check + отдавалка Base64 подписки)
+# Встроенный веб-сервер для проверок Amvera
 class HealthCheckHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path in ['/sub', '/sub/']:
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/plain; charset=utf-8')
-            self.end_headers()
-            base64_key = base64.b64encode(STATIC_SERVER_KEY.encode('utf-8'))
-            self.wfile.write(base64_key)
-        else:
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b"OK")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
 
 def run_web_server():
     server = HTTPServer(('0.0.0.0', 80), HealthCheckHandler)
@@ -43,6 +26,16 @@ threading.Thread(target=run_web_server, daemon=True).start()
 
 TOKEN = '8668630984:AAEQgKGPaJbrX-cgkLH62_MlLPdjaseDwtA'
 bot = telebot.TeleBot(TOKEN)
+
+# Сервер VLESS Desentom VPN
+STATIC_SERVER_KEY = (
+    "vless://342f746b-26f3-4093-b87e-bfdb6a38def2@178.248.236.7:50443"
+    "?security=reality&encryption=none&pbk=r-_zNu0BrD8PgGzg-KQSes06Si83txHWYPH-o8xXihk"
+    "&fp=qq&type=tcp&flow=xtls-rprx-vision&sni=rutube.ru&sid=a66ffc56b634bd44#Desentom%20VPN"
+)
+
+# Новая ссылка подписки Cloudflare Worker
+SUB_URL = 'https://desentom-vpn-bot.axelitvari.workers.dev/'
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
